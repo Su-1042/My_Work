f3b112f1019f1440f1d061525726aaf6544c6975
Su_1042 <sujal.deoda@research.iiit.ac.in>
2023-06-08 16:19:27 +0530

    q1done

--- Work/DSA/DSA_assn/DsaAss5/q1.c
+++ Work/DSA/DSA_assn/DsaAss5/q1.c
@@ -2,119 +2,58 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define MAX 1000000
+#define MIN -1
+
 typedef struct stHeap *Heap;
 // Queue to store the nodes during traversal
 typedef struct stnode *Node;
 typedef struct stgraph *Graph;
 typedef int vertex;
-typedef struct que *Que;
-typedef int elementType;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
 struct stHeap
 {
-     int current;
-     int capacity;
-     int *elements;
-};
-
-struct que
-{
+    int current;
     int capacity;
-    int front;
-    int rear;
-    int count;
-    elementType *arrayeletype;
+    int *elements;
 };
 
-Que quinit(int maxelem)
-{
-    Que Q;
-    Q = (Que)malloc(sizeof(struct que));
-    Q->arrayeletype = (int *)malloc(maxelem * sizeof(int));
-    Q->front = 0;
-    Q->rear = -1;
-    Q->count = 0;
-}
-
-int IsQueFull(Que Q)
-{
-    return (Q->count == Q->capacity);
-}
-
-int IsQueEmpty(Que Q)
-{
-    return (Q->count == 0);
-}
-
-void Enque(Que Q, elementType e)
-{
-    if (IsQueFull(Q))
-    {
-        printf("queue is already full\n");
-        return;
-    }
-    else
-    {
-        Q->rear = (Q->rear + 1) % (Q->capacity);
-        Q->arrayeletype[Q->rear] = e;
-        Q->count++;
-    }
-}
-
-elementType Deque(Que Q)
-{
-    elementType e;
-    if (IsQueEmpty)
-    {
-        printf("queue is already empty\n");
-    }
-    else
-    {
-        e = Q->arrayeletype[Q->front];
-        Q->front = (Q->front + 1) % (Q->capacity);
-        return e;
-    }
-}
-
-void printqueue(Que Q)
-{
-    int temp = Q->front;
-    while (Q->rear == temp)
-    {
-        printf("%d", Q->arrayeletype[temp]);
-        temp = (temp + 1) % (Q->capacity);
-    }
-}
-
-
-Heap CreateHeap( int cap)
+Heap CreateHeap(int cap)
 {
     Heap H;
     H = (Heap)malloc(sizeof(struct stHeap));
     H->capacity = cap;
-    H->elements = ( int *)malloc(sizeof( int) * (cap + 5));
+    H->elements = (int *)malloc(sizeof(int) * (cap));
     H->current = 0;
-    H->elements[0] = -999999;
+    H->elements[0]=-999999;
+
     return H;
 }
 
- int IsHeapFull(Heap H)
+int IsFull(Heap H)
 {
     return (H->current == H->capacity);
 }
 
- int IsHeapEmpty(Heap H)
+int IsEmpty(Heap H)
 {
     return (H->current == 0);
 }
 
-void InsertHeap(Heap H,  int x)
+void InsertHeap(Heap H, int x)
 {
-     int i;
+    int i;
 
-    if (IsHeapFull(H))
+    if (IsFull(H))
     {
-        // printf("heap is full\n");
+        printf("heap is full\n");
         return;
     }
 
@@ -127,11 +66,23 @@ void InsertHeap(Heap H,  int x)
     return;
 }
 
- int MinEle(Heap H)
+struct stgraph
 {
-     int i, child;
-     int minelement, last;
-    if (IsHeapEmpty(H))
+    vertex n;
+    Node *pvertex;
+};
+
+struct stnode
+{
+    vertex val;
+    Node pnext;
+};
+
+int MinEle(Heap H)
+{
+    int i, child;
+    int minelement, last;
+    if (IsEmpty(H))
     {
         return H->elements[0];
     }
@@ -159,35 +110,6 @@ void InsertHeap(Heap H,  int x)
     return minelement;
 }
 
-void FreeHeap(Heap H)
-{
-    free(H->elements);
-    free(H);
-}
-
-void FreeArr(int **arr)
-{
-    for (int i = 0; i < 2; i++)
-    {
-        free(arr[i]);
-    }
-
-    free(arr);
-}
-
-
-
-struct stgraph
-{
-    vertex n;
-    Node *pvertex;
-};
-
-struct stnode
-{
-    vertex val;
-    Node pnext;
-};
 
 Graph CreateGraph(int numnodes)
 {
@@ -202,7 +124,7 @@ Graph CreateGraph(int numnodes)
     {
         G->pvertex[i] = (Node)malloc(sizeof(struct stnode));
         assert(G->pvertex[i] != NULL);
-        G->pvertex[i]->val = i+1;
+        G->pvertex[i]->val = i;
         G->pvertex[i]->pnext = NULL;
     }
 
@@ -217,32 +139,30 @@ void InsertEdge(Graph G, vertex u, vertex v)
     Node temp;
     temp = G->pvertex[u];
 
-    while (temp->pnext != NULL)
+    if (temp->pnext == NULL)
     {
-        if (temp->pnext->val == v)
-        {
-            found = 1;
-            break;
-        }
-        temp = temp->pnext;
+        // printf("new element\n");
+        temp = (Node)malloc(sizeof(struct stnode));
+        temp->val = v;
+        G->pvertex[u]->pnext = temp;
     }
+
+    // finding if exists
 
     // it was not found so we will insert a node at the beginng to insert at the end use the methid that
     // u used in assignment twio where you maintained an array of node pointers to maintain a node pointer
     // at the end of each node in the graph
-    if (found == 0)
+    // if (found == 0)
     {
         temp = (Node)malloc(sizeof(struct stnode));
         assert(temp != NULL);
         temp->val = v;
         temp->pnext = G->pvertex[u]->pnext;
-        G->pvertex[u] = temp;
+        G->pvertex[u]->pnext = temp;
     }
 
     return;
 }
-
-
 
 int main()
 {
@@ -253,88 +173,85 @@ int main()
     scanf("%d", &num_nodespossible);
     scanf("%d", &numpaths);
 
-    G=CreateGraph(num_nodespossible);
+    G = CreateGraph(num_nodespossible);
 
     int **arr;
     arr = (int **)malloc(sizeof(int *) * numpaths);
 
     for (int i = 0; i < numpaths; i++)
     {
-        arr[i]=(int*)malloc(sizeof(int)*2);
+        arr[i] = (int *)malloc(sizeof(int) * 2);
     }
 
     for (int i = 0; i < numpaths; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            scanf("%d", &arr[i][j]);
+            int x;
+            scanf("%d", &x);
+            x -= 1;
+            arr[i][j] = x;
         }
     }
 
+    // printf("elements scanned\n");
 
-    for (int i=0;i<numpaths;i++)
+    for (int i = 0; i < numpaths; i++)
     {
-        InsertEdge(G,arr[i][0],arr[i][1]);
-        InsertEdge(G,arr[i][1],arr[i][0]);
+        InsertEdge(G, arr[i][0], arr[i][1]);
+        InsertEdge(G, arr[i][1], arr[i][0]);
+        // printf("edge added\n");
     }
 
+    // printf("adj mat created\n");
 
-    int * visitedarr;
-    visitedarr=(int*)calloc(sizeof(int),num_nodespossible+1);
+    int *visitedarr;
+    visitedarr = (int *)malloc(sizeof(int) * num_nodespossible);
 
-    Que Q;
-    Q=quinit(num_nodespossible); 
+    for (int i = 0; i < num_nodespossible; i++)
+    {
+        visitedarr[i] = 0;
+    }
+
+    // Que Q;
+    // Q = quinit(num_nodespossible);
 
     Heap H;
-    H=CreateHeap(num_nodespossible);
+    H = CreateHeap(2*num_nodespossible);
+    // printf("x\n");
 
-    Enque(Q,1);
-    printf("1 ");
-    int trackQ=0;
-    // the values stored are from 1 to n
-    while (trackQ!=(num_nodespossible-1))    {
+    InsertHeap(H, 0);
 
-        // adding the elements to a queue traversing the bredth
-            Node temp;
-            temp=G->pvertex[trackQ];
+    int n = 0;
+    int minele = 0;
+    visitedarr[0] = 1;
 
-            while(temp->pnext!=NULL)
-            {
-                if (visitedarr[temp->val]==0)
-                {
-                    InsertHeap(H,temp->val);
-                    Enque(Q,visitedarr[temp->val]);
-                    visitedarr[temp->val]=1;
-                }
-                temp=temp->pnext;
-            }
-
-            printf("%d ",MinEle(H));
-        
-
-        trackQ++;
-    }
-
-
-    printqueue(Q);
-
-    while (!IsHeapEmpty)
+    while (n < num_nodespossible)
     {
-        printf("%d ",MinEle(H));
+        Node temp;
+
+        minele=MinEle(H);
+        printf("%d ", minele + 1);
+        temp = (G->pvertex[minele]);
+
+        while (temp->pnext != NULL)
+        {
+            int u;
+
+            u = temp->pnext->val;
+            if (visitedarr[u] == 0)
+            {
+                visitedarr[u] = 1;
+                InsertHeap(H, u);
+            }
+            temp = temp->pnext;
+        }
+        
+        // minele = H->elements[0];
+        n++;
     }
+    // printf("%d\n",num_nodespossible);
 
     printf("\n");
-
-
-    // for (int i = 0; i < numpaths; i++)
-    // {
-    //     for (int j = 0; j < 2; j++)
-    //     {
-    //         printf("%d ", arr[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-
-    FreeHeap(H);
-    FreeArr(arr);
 }
+
